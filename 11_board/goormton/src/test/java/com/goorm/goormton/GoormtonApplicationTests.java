eafcfd0645f67e5d562c4132211e51678866b074
SPIDEY965 <bbnt25@gmail.com>
2023-10-27 11:02:07 +0900

    feat : new project

---
+++ 11_board/goormton/src/test/java/com/goorm/goormton/GoormtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.goormton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
